0510458c7e9da2519a336e559820e1d4455199fc
aaron <boddy.jo.bj@gmail.com>
2025-07-08 12:48:26 +0800

    updated ASSN1_Ans

--- Assignments_Answer/ASSN1_Ans/GravityCalculator.java
+++ Assignments_Answer/ASSN1_Ans/GravityCalculator.java
@@ -4,7 +4,7 @@ class GravityCalculator {
         double initialVelocity = 0.0;
         double fallingTime = 10.0;
         double initialPosition = 0.0;
-        double finalPosition = 0.0;
+        double finalPosition = 0.5 * gravity * fallingTime * fallingTime + initialVelocity * fallingTime + initialPosition;
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
