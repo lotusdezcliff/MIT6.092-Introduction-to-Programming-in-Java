2768fd5595fb617ea937bb55a61c58d5170b9e38
aaron <boddy.jo.bj@gmail.com>
2025-07-29 17:25:48 +0800

    completed Assignment5

--- Assignments_Answer/ASSN5_Ans/DrawGraphics.java
+++ Assignments_Answer/ASSN5_Ans/DrawGraphics.java
@@ -1,12 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class DrawGraphics {
-    BouncingBox box;
-    
+    ArrayList<BouncingBox> boxes = new ArrayList<>();
     /** Initializes this class for drawing. */
     public DrawGraphics() {
-        box = new BouncingBox(200, 50, Color.RED);
+        BouncingBox box1 = new BouncingBox(200, 50, Color.RED);
+        box1.setMovementVector(1, 0);
+        BouncingBox box2 = new BouncingBox(100, 50, Color.BLUE);
+        box2.setMovementVector(0, 1);
+        BouncingBox box3 = new BouncingBox(300, 50, Color.GREEN);
+        box3.setMovementVector(1, 1);
+        
+        boxes.add(box1);
+        boxes.add(box2);
+        boxes.add(box3);
     }
 
     /** Draw the contents of the window on surface. Called 20 times per second. */
@@ -14,7 +23,8 @@ public class DrawGraphics {
         surface.drawLine(50, 50, 250, 250);
         surface.drawOval(100, 100, 150, 150);
         surface.drawRect(20, 20, 30, 30);
-        box.draw(surface);
+        for (BouncingBox box : boxes) {
+            box.draw(surface);}
         surface.drawPolygon(new int[]{50, 100, 75}, new int[]{50, 100, 75}, 3);
     }
 
