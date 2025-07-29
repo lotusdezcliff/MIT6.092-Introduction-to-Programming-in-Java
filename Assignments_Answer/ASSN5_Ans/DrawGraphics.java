602dfd2a527b4d85ba474af3d8719af33c2df437
aaron <boddy.jo.bj@gmail.com>
2025-07-29 12:30:07 +0800

    completed Assignment5 part one

--- Assignments_Answer/ASSN5_Ans/DrawGraphics.java
+++ Assignments_Answer/ASSN5_Ans/DrawGraphics.java
@@ -12,6 +12,10 @@ public class DrawGraphics {
     /** Draw the contents of the window on surface. Called 20 times per second. */
     public void draw(Graphics surface) {
         surface.drawLine(50, 50, 250, 250);
+        surface.drawOval(100, 100, 150, 150);
+        surface.drawRect(20, 20, 30, 30);
         box.draw(surface);
+        surface.drawPolygon(new int[]{50, 100, 75}, new int[]{50, 100, 75}, 3);
     }
+
 } 
