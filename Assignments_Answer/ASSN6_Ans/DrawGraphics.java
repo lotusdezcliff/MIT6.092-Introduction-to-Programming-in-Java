37deb6062582f3e112930e58b3aec79eaea85044
aaron <boddy.jo.bj@gmail.com>
2025-08-20 06:32:25 +0800

    Finished Assn6

--- Assignments_Answer/ASSN6_Ans/DrawGraphics.java
+++ Assignments_Answer/ASSN6_Ans/DrawGraphics.java
@@ -3,30 +3,25 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class DrawGraphics {
-    ArrayList<Bouncer> bouncers;
-    ArrayList<StraightMover> straightMovers;
+    ArrayList<Mover> movers;
 
-    /** Initializes this class for drawing. */
     public DrawGraphics() {
-        bouncers = new ArrayList<>();
-        straightMovers = new ArrayList<>();
-        bouncers.add(new Bouncer(100, 170, new Rectangle(30, 20, Color.RED)));
-        bouncers.add(new Bouncer(50, 70, new Oval(40, 30, Color.BLUE)));
-        straightMovers.add(new StraightMover(20, 40, new Rectangle(30, 20, Color.YELLOW)));
-        straightMovers.add(new StraightMover(55, 75, new Oval(70, 80, Color.GREEN)));
-        bouncers.get(0).setMovementVector(3, 1);
-        bouncers.get(1).setMovementVector(2, 2);
-        straightMovers.get(0).setMovementVector(1,1);
-        straightMovers.get(1).setMovementVector(1,1);
+        movers = new ArrayList<>();
+        
+        movers.add(new Bouncer(100, 170, new Rectangle(15, 20, Color.RED)));
+        movers.get(0).setMovementVector(3, 1);
+        movers.add(new Bouncer(50, 70, new Oval(30, 20, Color.BLUE)));
+        movers.get(1).setMovementVector(2, 2);
+        
+        movers.add(new StraightMover(20, 40, new Rectangle(15, 20, Color.YELLOW)));
+        movers.get(2).setMovementVector(1, 1);
+        movers.add(new StraightMover(55, 75, new Oval(30, 20, Color.GREEN)));
+        movers.get(3).setMovementVector(1, 1);
     }
 
-    /** Draw the contents of the window on surface. */
     public void draw(Graphics surface) {
-        for (Bouncer b : bouncers) {
-            b.draw(surface);
+        for (Mover m : movers) {
+            m.draw(surface);
         }
-        for (StraightMover s : straightMovers) {
-            s.draw(surface);
-        } 
     }
 }
